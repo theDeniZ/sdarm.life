216fb1c07b97626cbd5598c4890777e40a8913a9
theDeniZ <theDeniZ@icloud.com>
2026-03-10 10:45:36 +0100

    chore: fix cloudflare build
    Signed-off-by: theDeniZ <theDeniZ@icloud.com>

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -1,10 +1,9 @@
 // apps/web/next.config.ts  (same for apps/admin/next.config.ts)
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
-import type { NextConfig } from 'next';
 
 if (process.env.NODE_ENV === 'development') {
   await setupDevPlatform();
 }
 
-const nextConfig: NextConfig = {};
+const nextConfig = {};
 export default nextConfig;
