9c23a63c58add82faa23027deb87d03de540e40c
theDeniZ <theDeniZ@icloud.com>
2026-05-08 14:48:20 +0200

    chore: update node
    Signed-off-by: theDeniZ <theDeniZ@icloud.com>

---
+++ release.config.cjs
@@ -0,0 +1,33 @@
+'use strict';
+
+const isRc = process.env.RELEASE_CHANNEL === 'rc';
+
+/** @type {import('semantic-release').GlobalConfig} */
+module.exports = {
+  branches: [
+    'main',
+    { name: 'develop', prerelease: 'rc' },
+  ],
+  plugins: isRc
+    ? [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        ['@semantic-release/npm', { npmPublish: false }],
+        ['@semantic-release/git', {
+          assets: ['package.json'],
+          message: 'chore(release): ${nextRelease.version} [skip ci]',
+        }],
+        '@semantic-release/github',
+      ]
+    : [
+        '@semantic-release/commit-analyzer',
+        '@semantic-release/release-notes-generator',
+        ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
+        ['@semantic-release/npm', { npmPublish: false }],
+        ['@semantic-release/git', {
+          assets: ['CHANGELOG.md', 'package.json'],
+          message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
+        }],
+        '@semantic-release/github',
+      ],
+};
